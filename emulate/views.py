69d027a507092e7eee6637853bfb21901ea5ae29
Philip Geurin <Philip.Geurin@gmail.com>
2015-10-14 23:40:35 -0500

    set so that board is not taken in. Try again with input logic.

--- emulate/views.py
+++ emulate/views.py
@@ -396,7 +396,7 @@ from django.template import RequestContext, loader
 def index(request):
 	#take in the entry
 	#if ('q' in request.GET) and request.GET['q'].strip():
-    if ('q' in request.GET) and ('b' in request.GET) and ('nextPlay' != ''):
+    if (len('q')>0) and (len('nextPlay')>0):
                 #LATER: check to make sure nextPlay is a integer between 0 and 6.
         #build the board
         q = request.GET['q']
